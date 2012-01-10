f1fffc33a28825aef2fc635c978aca12b99ffd9c
Justin Reese <justin.x.reese@gmail.com>
2012-01-10 00:12:31 -0500

    Don't need the array

--- palindromes.rb
+++ palindromes.rb
@@ -1,11 +1,9 @@
 def find_longest_palindrome(string)
-  p = []
   string.length.downto(0) { |i|
     0.upto(string.length) { |j|
-      p << string[j,i] if string[j,i] == string[j,i].reverse && string[j,i].size > 1
+      return string[j,i] if string[j,i] == string[j,i].reverse && string[j,i].size > 1
     }
   }
-  p.sort_by(&:length).last
 end
 
 str = "abacdfgdcabaypqqpy"
